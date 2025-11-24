06cb8f066d7391954b1ef576b0ae89b9453e4e03
Naught33 <nevillevalentine56@gmail.com>
2025-11-24 06:56:09 +0300

    fixed configs

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
         explore: resolve(__dirname, 'explore.html'),
         account: resolve(__dirname, 'account.html'),
         blogread: resolve(__dirname, 'blogread.html'),
-        loginsignup: resolve(__dirname, 'loginsignup.html')
+        loginsignup: resolve(__dirname, 'loginsignup.html'),
+        allblogs: resolve(__dirname, 'admin/allblogs.html'),
+        writeblog: resolve(__dirname, 'admin/writeblog.html')
       }
     }
   }
